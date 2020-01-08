6b83608a2efcae8027ed7aa7cd9d096d987c45cd
Leandro Shoiti <56648792+Shoiti21@users.noreply.github.com>
2020-01-08 10:40:37 -0300

    Add files via upload

---
+++ Exer_1_1_7_2020/src/br/com/gft/main/Main.java
@@ -0,0 +1,68 @@
+package br.com.gft.main;
+import java.util.Scanner;
+import br.com.gft.model.Veiculo;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		String marca="Hyundai";
+		String modelo="HB20";
+		String placa="2222";
+		String cor="Preto";
+		boolean isligado=false;
+		int qtdabastecer;
+		int combustivel=100;
+		int velocidade=0;
+		int preco=2;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("LIGAR/DESLIGAR(resposta boolean):");
+		isligado=sc.nextBoolean();
+		
+		while(isligado==true && combustivel>0) {
+			boolean contrFrear=false;
+			boolean contrPintar=false;
+			
+			System.out.println("Você deseja abastecer quanto?");
+			qtdabastecer=sc.nextInt();
+			System.out.println("Você que frear?(resposta boolean)");
+			contrFrear=sc.nextBoolean();
+			System.out.println("Você que pintar seu veículo?(resposta boolean)");
+			contrPintar=sc.nextBoolean();
+			if(contrPintar==true) {
+				System.out.println("Qual cor?");
+				cor=sc.next();
+			}
+			
+			Veiculo meuveiculo = new Veiculo(marca,modelo,placa,cor,isligado,combustivel,velocidade,preco);
+			meuveiculo.acelerar();
+			meuveiculo.abastecer(qtdabastecer);
+			
+			if(contrFrear==true) {
+				meuveiculo.frear();
+			}
+			if(contrPintar==true) {
+				meuveiculo.pintar(cor);
+			}
+			meuveiculo.pintar(cor);
+			if(isligado==true) {
+				meuveiculo.ligar();
+			}
+			else {
+				meuveiculo.desligar();
+			}
+			
+			
+			System.out.println("STATUS DO VEÍCULO");
+			System.out.println("Marca: "+marca);
+			System.out.println("Modelo: "+modelo);
+			System.out.println("Placa: "+placa);
+			System.out.println("Cor: "+meuveiculo.getCor());
+			System.out.print("O veículo está "+meuveiculo.getisLigado());
+			System.out.println(" com velocidade de "+meuveiculo.getVelocidade()+"Km/h.");
+			System.out.println("Está com "+meuveiculo.getLitrosCombustivel()+" litros de combustivel.");
+			System.out.println("_________________________________________________________________________________");
+		}
+	}
+}
---
+++ Exer_1_1_7_2020/src/br/com/gft/model/Veiculo.java
@@ -0,0 +1,74 @@
+package br.com.gft.model;
+import java.util.Scanner;
+
+public class Veiculo {
+	private String Marca, Modelo, Placa, Cor;
+	private float Km;
+	private boolean isLigado;
+	private int LitrosCombustivel, Velocidade;
+	private double Preco;
+	
+	//construtor
+	public Veiculo(String marca, String modelo, String placa, String cor, boolean isligado, int combustivel, int velocidade, double preco){
+		this.Marca=marca;
+		this.Modelo=modelo;
+		this.Placa=placa;
+		this.Cor=cor;
+		this.isLigado=isligado;
+		this.LitrosCombustivel=combustivel;
+		this.Velocidade=velocidade;
+		this.Preco=preco;
+	}
+	//métodos
+	public void acelerar() {
+		if(this.isLigado==true) {
+			this.LitrosCombustivel--;
+			this.Velocidade=20;
+		}
+	}
+	public void abastecer(int qtdLitros) {
+		if(this.LitrosCombustivel<100) {
+			this.LitrosCombustivel+=qtdLitros;
+			if(this.LitrosCombustivel<0) {
+				this.LitrosCombustivel=0;	
+			}
+			if(this.LitrosCombustivel>100) {
+				this.LitrosCombustivel=100;
+			}
+		}
+	}
+	public void frear() {
+		if(this.isLigado==true) {
+			if(this.Velocidade>0) {
+				this.Velocidade-=10;
+			}
+		}
+	}
+	public void pintar(String cor) {
+		this.Cor=cor;
+	}
+	public void ligar() {
+		if(this.isLigado==false) {
+			this.isLigado=true;
+		}
+	}
+	public void desligar() {
+		if(this.isLigado==true) {
+			this.isLigado=false;
+		}
+	}
+	
+	//get e set
+	public String getCor() {
+		return this.Cor;
+	}
+	public boolean getisLigado() {
+		return this.isLigado;
+	}
+	public int getLitrosCombustivel() {
+		return this.LitrosCombustivel;
+	}
+	public int getVelocidade() {
+		return this.Velocidade;
+	}
+}
